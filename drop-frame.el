0e8a0a82a34347477daa9089bc02b246c5a05d45
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2023-08-01 10:09:31 +0200

    rewrite without eieio

--- drop-frame.el
+++ drop-frame.el
@@ -5,10 +5,10 @@
 ;; Author: Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
 ;; Maintainer: Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
 ;; Created: July 16, 2023
-;; Modified: July 16, 2023
-;; Version: 0.0.1
+;; Modified: August 1, 2023
+;; Version: 0.0.4
 ;; Keywords: Symbol’s value as variable is void: finder-known-keywords
-;; Homepage: https://github.com/antoniopetrillo/drop-frame
+;; Homepage: https://github.com/antonio-petrillo/drop-frame
 ;; Package-Requires: ((emacs "24.3"))
 ;;
 ;; This file is not part of GNU Emacs.
@@ -19,62 +19,53 @@
 ;;
 ;;; Code:
 
-(defvar drop-frame--previous-focus nil)
+(defvar drop-frame--previous-focus nil
+  "This var store the focus of the frame that open a drop frame, so once the drop frame is hided it could be restored properly.")
 
-;;;###autoload
-(defclass drop-frame ()
-  ((name :initarg :name
-         :type string
-         :custom string
-         :documentation "drop-frame name")
-   (frame :type frame
-          :documentation "drop-frame frame")
-   (frame-params :type list
-                 :initform '()
-                 :initarg :frame-params
-                 :documentation "drop-frame frame parameters")
-   (setup-fns :initarg :setup-fns
-             :type list
-             :documentation "drop-frame setup functions")))
-
-;;;###autoload
-(cl-defmethod drop-frame--create ((a-frame drop-frame))
-  (let ((focus (selected-frame))
-        (fr (make-frame (oref a-frame frame-params))))
-        (progn
-          (setq drop-frame--previous-focus focus)
-          (oset a-frame frame fr)
-          (mapc #'funcall (oref a-frame setup-fns)))))
-
-;;;###autoload
-(cl-defmethod drop-frame--show ((a-frame drop-frame))
-  "toggle the drop-frame"
+(defun drop-frame--show (a-frame)
+  "Given a frame make it visible"
   (progn
     (setq drop-frame--previous-focus (selected-frame))
-    (make-frame-visible (oref a-frame frame))))
+    (make-frame-visible a-frame)))
 
-;;;###autoload
-(cl-defmethod drop-frame--hide ((a-frame drop-frame))
-  "toggle the drop-frame"
+(defun drop-frame--hide (a-frame)
+  "Given a frame make it invisible"
   (progn
     (select-frame-set-input-focus drop-frame--previous-focus)
     (setq drop-frame--previous-focus nil)
-    (make-frame-invisible (oref a-frame frame) t)))
+    (make-frame-invisible a-frame t)))
 
-;;;###autoload
-(cl-defmethod drop-frame--toggle-private ((a-frame drop-frame))
-  (if (not (slot-boundp a-frame 'frame))
-      (drop-frame--create a-frame)
-    (let ((fr (oref a-frame frame)))
-      (cond ((or (not fr) (not (frame-live-p fr))) (drop-frame--create a-frame))
-            ((frame-visible-p fr) (drop-frame--hide a-frame))
-            (:else (drop-frame--show a-frame))))))
-
-;;;###autoload
 (defun drop-frame--toggle (a-frame)
-  (lambda ()
-    (interactive)
-    (drop-frame--toggle-private a-frame)))
+  "Toggle a frame between visible and invisible."
+  (if (frame-visible-p a-frame)
+      (drop-frame--hide a-frame)
+    (drop-frame--show a-frame)))
+
+;; SEE: yequake
+(defun drop-frame--act (setup-action)
+  "Function used to setupe a new drop frame with the given actions, stolen from yequake"
+  (cl-typecase setup-action
+    (string (or (get-buffer setup-action)
+                (find-buffer-visiting setup-action)
+                (find-file-noselect setup-action)))
+    (function (funcall setup-action))))
+
+;;;###autoload
+(defun drop-frame--create (name setup-fns &optional frame-params)
+  "Create a new drop frame with the given parameters"
+  (let ((new-frame nil))
+    (lambda ()
+      (interactive)
+      (if (and new-frame (frame-live-p new-frame))
+          (drop-frame--toggle new-frame)
+        (let ((focus (selected-frame))
+              (frame-name (concat name " - drop frame")))
+          (progn
+            (setq new-frame (make-frame frame-params))
+            (set-frame-name frame-name)
+            (make-frame-visible new-frame)
+            (mapc #'drop-frame--act setup-fns)
+            (setq drop-frame--previous-focus focus)))))))
 
 (provide 'drop-frame)
 ;;; drop-frame.el ends here
